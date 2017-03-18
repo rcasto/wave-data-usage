15909a97ec6de1f636668eed59c19332995056a5
Richie Casto <Richie Casto>
2017-03-18 12:51:40 -0700

    cleanup and move more to config

--- main.js
+++ main.js
@@ -28,25 +28,42 @@ var dataUseageRequestOptions = {
     encoding: 'binary'
 };
 
-function getDataUsage() {
-    helpers.createSecureRequest(loginRequestOptions)
-        .then((responseObj) => {
-            dataUseageRequestOptions.headers['Cookie'] = responseObj.response.headers['set-cookie'];
-            return helpers.createSecureRequest(dataUseageRequestOptions);
-        })
-        .then((responseObj) => {
-            var dateString = (new Date()).toDateString().split(' ').join('-');
-            fs.writeFile(`./data/usage-as-of-${dateString}.webp`, responseObj.data, 'binary', (err) => {
-                if (err) {
-                    throw err
-                }
-                console.log('Data usage fetched and saved.');
-            });
-        })
+function getAndSaveDataUsage(path = `${config.dataStore}/${config.dataFilePrefix}.webp`) {
+    path = pathTransform(path);
+    return getDataUsage()
+        .then((dataUsage) => saveDataUsage(path, dataUsage))
+        .then(() => console.log(`Wrote data usage to ${path}`))
         .catch((error) => console.error(error));
 }
 
+function getDataUsage() {
+    return helpers.createSecureRequest(loginRequestOptions)
+        .then((responseObj) => {
+            dataUseageRequestOptions.headers['Cookie'] = responseObj.response.headers['set-cookie'];
+            return helpers.createSecureRequest(dataUseageRequestOptions);
+        });
+}
+
+function saveDataUsage(path, dataUsage) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, dataUsage, 'binary', (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(dataUsage);
+        });
+    });
+}
+
+function pathTransform(path) {
+    console.log(`Incoming path: ${path}`);
+    var pathParts = path.split('.');
+    console.log(`Parts of path ${pathParts[0]} | ${pathParts[1]}`);
+    var dateString = (new Date()).toDateString().split(' ').join('-');
+    return `${pathParts[0]}-${dateString}.${pathParts[1]}`;
+}
+
 // Initially get data usage upon initialization (This will be overwritten with latest at the end of the day)
-getDataUsage();
+getAndSaveDataUsage();
 // CRON job to get data usage right before midnight each day
-cron.schedule(`59 55 23 * * *`, () => getDataUsage());
+cron.schedule(`${config.cronTab}`, getAndSaveDataUsage);
