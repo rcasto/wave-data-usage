3f3452a835a2c6ef8ae9abf6e5e939f48b412cad
Richie Casto <Richie Casto>
2017-03-17 23:26:27 -0700

    start up project

---
+++ main.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var waveHostName = 'secure.wavecable.com'
+var waveDataUsageUrl = waveBaseUrl + '/iam/usage/data';
+var waveLoginUrl = waveBaseUrl + '/iam/login';
+var waveIndexUrl = waveBaseUrl + '/iam/index';
+
+// First login to Wave IAM page
+http.request({
+    hostname: waveHostName,
+    protocol: 'https',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
